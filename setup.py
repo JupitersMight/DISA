4441011b8e055e5ec830296947d3cc0ae81c7e31
JupitersMight <jupitersmight1996@gmail.com>
2021-08-26 12:02:46 +0100

    Added documentation, added example in python and jupyter notebook, added built version of the project

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DISA-JupitersMight",
+    version="0.0.1",
+    author="L. Alexandre, R.S. Costa, R. Henriques",
+    author_email="leonardoalexandre@tecnico.ulisboa.pt",
+    description="A library used to assess the informative and discriminative properties of subspaces/patterns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["multi-item discretization", "prior-free discretization", "heterogeneous biological data", "data mining"],
+    url="https://github.com/JupitersMight/DISA",
+    project_urls={
+        "Bug Tracker": "https://github.com/JupitersMight/DI2/issues",
+    },
+    packages=['DISA'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
